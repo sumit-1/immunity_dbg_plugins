a7861053b43c6e5cbf5974bc321463e713acae1c
Sumit <ksiiitm@gmail.com>
2014-02-19 14:37:09 +0530

    Added Badchar finder

---
+++ bchunter.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+"""
+Sumit Kumar
+Badchar Hunter: Hunt for badchar in an exe.
+
+Alpha Version
+
+"""
+
+__VERSION__ = '0.1'
+
+DESC="""Hunt down badchars"""
+
+import immlib
+import struct
+import binascii
+
+imm = immlib.Debugger()
+
+def usage():
+	imm.log("!bchunter reg array_of_badchar")
+	imm.log("ex: !bchunter ESP \"\\x00\\x01\\x0a\"", focus=1)
+
+def filterBC(buf, badchar):
+	for c in badchar:
+		buf = buf.replace(c, '')
+	return buf
+
+def compare(buf, mem_buf):
+	for i in range(len(buf)):
+		if buf[i] != mem_buf[i]:
+			return buf[i]
+	return True
+
+def convertToHex(badchar):
+	a = ''
+	for i in xrange(0, len(badchar), 4):
+		a += badchar[i+3:i+5]
+	return binascii.unhexlify(a)
+
+def main(args):
+	if len(args) < 2:
+		usage()
+		return 'Try Harder!'
+	buf = ''
+	reg = args[0]
+	badchar = convertToHex(args[1])
+	imm.log('Badchars entered: %d' % (len(badchar)))
+	for i in range(0, 255):
+		c = struct.pack('h', i)[0]
+		buf += c
+	buf = filterBC(buf, badchar)
+	mem_buf = imm.readMemory(imm.getRegs()[reg], len(buf))
+	res = compare(buf, mem_buf)
+	if res != True:
+		return 'New Badchar Identified : \\x%s' % (binascii.hexlify(res))
+	else:
+		return 'All Badchars identified'
